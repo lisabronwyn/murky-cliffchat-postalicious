f7869289c6229291c725a9020220752218e45b43
Debrena McEwen <debrenamcewen@gmail.com>
2017-04-13 13:40:55 -0700

    add bodyParser and post route for url input

--- postalicious/server.js
+++ postalicious/server.js
@@ -2,12 +2,24 @@ const express = require('express')
 const server = express()
 const path = require('path')
 const http = require('http').createServer(server)
+const bodyParser = require('body-parser')
+const fetchUrl = require("fetch").fetchUrl
+
+
+server.use(bodyParser.json({ type: 'application/json' }))
+server.use(bodyParser.urlencoded({extended: true}))
 
 server.use('/', express.static(path.join(__dirname, 'public')))
 
-server.get('/', (request, response) => {
-  response.send('HELLO THEREEEEEEE')
+server.post('/getdata', (request, response) => {
+  console.log(request.body)
+  fetchUrl(request.body.url, function(error, meta, body){
+      console.log(body.toString())
+      response.json(meta)
+  })
 })
+
+
 
 server.listen(3001, (request, respose) => {
   console.log("LISTENING ON PORT 3001");
